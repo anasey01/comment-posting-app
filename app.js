689b5dd61b796ec89edea244904e0c3285b341cb
Oluseyi Anani <anasey2018@gmail.com>
2018-04-14 23:29:41 +0100

    Backend implementation

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var passport = require('passport');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//connect to mongodb
+mongoose.connect("mongodb://localhost:27017");
+require('./models/models.js');
+var api = require('./routes/api');
+var auth = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -14,13 +20,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(session({
+  secret: 'super duper secret'
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(express.static(__dirname + '/public'));     //serve static assets
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
+//Initialize models
+var mongoose = require('mongoose');                         //add for Mongo support
+mongoose.connect('mongodb://localhost/test-chirp');              //connect to Mongo
+
+app.use('/api', api);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
