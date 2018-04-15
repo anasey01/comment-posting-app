29fe80a6e445ea2dea4bf0e8c76d60e10ee3fd60
Oluseyi Anani <anasey2018@gmail.com>
2018-04-15 22:34:28 +0100

    Implemented Services, adjustments on authentications and frontend stuffs

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -19,7 +19,7 @@ module.exports = function(passport){
 		failureRedirect: '/auth/failure'
 	}));
 
-	//sign up
+	//Register
 	router.post('/signup', passport.authenticate('signup', {
 		successRedirect: '/auth/success',
 		failureRedirect: '/auth/failure'
